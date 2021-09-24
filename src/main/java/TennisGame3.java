a4f9b1684adeb946791967224599d3a52d5de9c9
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-24 17:20:19 -0500

    Se cambiaron los nombres de las variables y se elimino un else innecesario

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,8 +1,8 @@
 
 public class TennisGame3 implements TennisGame {
     
-    private int p2;
-    private int p1;
+    private int scorePlayer2;
+    private int scorePlayer1;
     private String p1N;
     private String p2N;
 
@@ -12,25 +12,23 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        String score;
+        if (scorePlayer1 < 4 && scorePlayer2 < 4 && !(scorePlayer1 + scorePlayer2 == 6)) {
+            String[] arrayScoreMessages = {"Love", "Fifteen", "Thirty", "Forty"};
+            score = arrayScoreMessages[scorePlayer1];
+            return (scorePlayer1 == scorePlayer2) ? score + "-All" : score + "-" + arrayScoreMessages[scorePlayer2];
         }
+        if (scorePlayer1 == scorePlayer2)
+            return "Deuce";
+        score = scorePlayer1 > scorePlayer2 ? p1N : p2N;
+        return ((scorePlayer1 - scorePlayer2)*(scorePlayer1 - scorePlayer2) == 1) ? "Advantage " + score : "Win for " + score;
     }
-    
+
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        if (playerName.equals("player1"))
+            this.scorePlayer1 += 1;
         else
-            this.p2 += 1;
-        
+            this.scorePlayer2 += 1;
     }
 
 }
